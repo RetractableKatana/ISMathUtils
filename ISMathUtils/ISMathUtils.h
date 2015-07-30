90076155deeab196c236a8403ed867d51b7c49f9
James Thompson <retractable.katana@gmail.com>
2015-07-30 00:14:22 -0700

    Initial Commit

---
+++ ISMathUtils/ISMathUtils.h
@@ -0,0 +1,19 @@
+//
+//  ISMathUtils.h
+//  ISMathUtils
+//
+//  Created by James Thompson on 7/30/15.
+//  Copyright (c) 2015 IntelligentSprite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ISMathUtils.
+FOUNDATION_EXPORT double ISMathUtilsVersionNumber;
+
+//! Project version string for ISMathUtils.
+FOUNDATION_EXPORT const unsigned char ISMathUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ISMathUtils/PublicHeader.h>
+
+
